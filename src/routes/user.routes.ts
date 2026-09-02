38b5677b08569c9634e9f0ce1ae324c423cf498d
anim710 <anim3654@gmail.com>
2026-09-02 19:50:18 +0600

    updated the user module (controller and router file)

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,11 +1,12 @@
 import { Router } from 'express';
+import { getUserBookmarks, getUserCommentedBlogs, getUserLikedBlogs, toggleBookmark } from '../controller/user.controller.js';
 
 const router = Router();
 
 // Routes for User Dashboard & Tracking Module
-// router.patch('/bookmark', toggleBookmark);
-// router.get('/bookmarks/:userId', getUserBookmarks);
-// router.get('/liked-blogs/:userId', getUserLikedBlogs);
-// router.get('/commented-blogs/:userId', getUserCommentedBlogs);
+router.patch('/bookmark', toggleBookmark);
+router.get('/bookmarks/:userId', getUserBookmarks);
+router.get('/liked-blogs/:userId', getUserLikedBlogs);
+router.get('/commented-blogs/:userId', getUserCommentedBlogs);
 
 export default router;
