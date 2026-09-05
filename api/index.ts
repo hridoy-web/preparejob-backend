e340803921b4684379bb373595e95f1093d14ace
Hridoy Chowdhury <t.okay383@gmail.com>
2026-09-05 14:27:10 +0600

    Fix database connection handling for serverless

--- api/index.ts
+++ api/index.ts
@@ -2,6 +2,7 @@ import app from "../src/app.js";
 import connectDB from "../src/config/db.js";
 
 // Vercel Serverless Execution DB Connection
-connectDB();
-
-export default app;
+export default async function handler(req: any, res: any) {
+  await connectDB();
+  return app(req, res);
+}
